ec664d2e47baaf1e2fd3a8d3f7fca8e0d2b4f963
Peter Harrison <16875803+palisadoes@users.noreply.github.com>
2025-11-11 00:09:31 -0500

    Update docusaurus.config.ts

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -187,7 +187,7 @@ const config: Config = {
       colorMode: {
         defaultMode: 'light',
         disableSwitch: false,
-        respectPrefersColorScheme: false,
+        respectPrefersColorScheme: true,
       },
       prism: {
         theme: prismThemes.github,
